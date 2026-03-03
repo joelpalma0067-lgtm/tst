7772891ef60943ca719d88b171dbbf22f86589dc
joelpalma0067-lgtm <joelpalma0067@gmail.com>
2026-03-02 22:06:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from email.message import EmailMessage
 
 # --- CONFIGURACIÓN ---
 MI_CORREO = "zerokhyt@gmail.com"
-MI_CLAVE_16 = "gkllljgczgycrwkd"
+MI_CLAVE_16 = "wblebsdaercnceav"
 
 def descargar_audio(asunto):
     # Buscamos solo el audio de mejor calidad
@@ -74,6 +74,7 @@ if __name__ == "__main__":
     while True:
         ejecutar_bot()
         time.sleep(30)
+
 
 
 
